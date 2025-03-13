5fd5d573cd72947423166ca5cedb378d93180ad3
plmenezes <lukemenezes06@gmail.com>
2025-03-12 21:49:53 -0300

    codigos da aula02

---
+++ aulas/aula01/imprimir_caracter.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+  printf("%c/n", "A");
+
+  printf("ola %s\n", "davi");
+return 0;
+}
---
+++ aulas/aula01/imprimir_decimais.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+   printf("%f\n", 8.5);
+   printf("%.2f\n", 3.141563);
+return 0;
+}
